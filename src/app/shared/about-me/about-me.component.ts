645a3102f9eca9ef3f0202349c71dda43eb05c05
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-08-21 12:23:05 +0700

    fix: add experience section

--- src/app/shared/about-me/about-me.component.ts
+++ src/app/shared/about-me/about-me.component.ts
@@ -6,7 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent implements OnInit {
-
+  myStack: Array<any> = [
+    {
+      tech: 'php',
+      style: 'btn-outline-primary'
+    },
+    {
+      tech: 'html',
+      style: 'btn-outline-success'
+    },
+    {
+      tech: 'css',
+      style: 'btn-outline-danger'
+    },
+    {
+      tech: 'javascript',
+      style: 'btn-outline-warning'
+    },
+    {
+      tech: 'typescript',
+      style: 'btn-outline-primary'
+    },
+    {
+      tech: 'wordpress',
+      style: 'btn-outline-info'
+    },
+    {
+      tech: 'laravel',
+      style: 'btn-outline-danger'
+    },
+    {
+      tech: 'figma',
+      style: 'btn-outline-primary'
+    },
+    {
+      tech: 'adobe xd',
+      style: 'btn-outline-dark'
+    },
+    {
+      tech: 'inkscape',
+      style: 'btn-outline-secondary'
+    },
+    {
+      tech: 'gimp',
+      style: 'btn-outline-success'
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
