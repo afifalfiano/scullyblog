42dd5c6144fedd51690035157788d1f373d77367
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-08-24 23:31:51 +0700

    fix: add js from bootstrap

--- scully.scully-blog-test.config.ts
+++ scully.scully-blog-test.config.ts
@@ -4,10 +4,13 @@ import { DisableAngular } from 'scully-plugin-disable-angular';
 import { getHttp404Plugin } from '@gammastream/scully-plugin-http404';
 import '@notiz/scully-plugin-rss';
 
+require('scully-plugin-page-title');
+
+
 const { MinifyHtml } = require('scully-plugin-minify-html');
 
 const Http404Plugin = getHttp404Plugin();
-const postRenderersBlog = [DisableAngular, Http404Plugin, MinifyHtml, 'combineStylesAmpPlugin'];
+const postRenderersBlog = [DisableAngular, Http404Plugin, MinifyHtml, 'combineStylesAmpPlugin', 'changeTitlePlugin'];
 
 setPluginConfig('md', { enableSyntaxHighlighting: true });
 setPluginConfig(DisableAngular, 'render', {removeState: false});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { NavigationEnd, Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { filter } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
 // tslint:disable-next-line:ban-types
 
 @Component({
@@ -12,13 +13,18 @@ export class AppComponent {
   title = 'afifalfiano';
 
   constructor(
-    private router: Router
+    private router: Router,
+    private titleSvc: Title
   ) {
-    // this.router.events.subscribe(event => {
-    //   if (event instanceof NavigationEnd) {
-    //     (window as any).ga('set', 'page', event.urlAfterRedirects);
-    //     (window as any).ga('send', 'pageview');
-    //   }
-    // });
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        (window as any).ga('set', 'page', event.urlAfterRedirects);
+        (window as any).ga('send', 'pageview');
+      }
+    });
+  }
+
+  public setTitle(newTitle: string = 'Afif Alfiano'): any {
+    this.titleSvc.setTitle(newTitle);
   }
 }
