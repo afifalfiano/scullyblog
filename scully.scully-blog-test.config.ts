8aa20f6f85241790099cc40ca84a4a4bd9e953b2
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-08-24 22:52:13 +0700

    fix: fix lib

--- scully.scully-blog-test.config.ts
+++ scully.scully-blog-test.config.ts
@@ -1,13 +1,25 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import 'prismjs/components/prism-java.js';
+import { DisableAngular } from 'scully-plugin-disable-angular';
+import { getHttp404Plugin } from '@gammastream/scully-plugin-http404';
+import '@notiz/scully-plugin-rss';
+
+const { MinifyHtml } = require('scully-plugin-minify-html');
+
+const Http404Plugin = getHttp404Plugin();
+const postRenderersBlog = [DisableAngular, Http404Plugin, MinifyHtml, 'combineStylesAmpPlugin'];
+
+setPluginConfig('md', { enableSyntaxHighlighting: true });
+setPluginConfig(DisableAngular, 'render', {removeState: false});
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "scully-blog-test",
+  projectRoot: './src',
+  projectName: 'scully-blog-test',
   outDir: './dist/static',
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
       slug: {
-        folder: "./blog"
+        folder: './blog'
       }
     },
   }
