3832cb3c01fd7eb64b15cac4aa23cd98ab4d791c
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-08-29 18:03:33 +0700

    fix: update experience

--- src/app/shared/experience/experience.component.ts
+++ src/app/shared/experience/experience.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ExperienceComponent implements OnInit {
   myExperience: Array<any> = [
     {
-      position: 'Frontend Developer',
+      position: 'Frontend Dev',
       time: '20 August, 2020',
       description: 'Implementation of a design product to the web application. (On the frontend developer, we use an Angular framework.'
     },
