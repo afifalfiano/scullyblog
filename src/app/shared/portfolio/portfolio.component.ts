a115bcd0ac3d10017e8322c03bca3267796b2fb2
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-08-29 18:02:33 +0700

    fix: update portfolio

--- src/app/shared/portfolio/portfolio.component.ts
+++ src/app/shared/portfolio/portfolio.component.ts
@@ -10,18 +10,39 @@ export class PortfolioComponent implements OnInit {
     {
       asset: './assets/portfolio/pwa-pokemon.jpg',
       title: 'Pokemon Card Apps',
-      description: 'Build an application using the angular framework with pokemon api. Also uses lazy loading images and PWAs to improve website performance'
+      description: 'Build an application using the angular framework with pokemon api. Also uses lazy loading images and PWAs to improve website performance',
+      url: 'https://github.com/afifalfiano/pokemonCard'
     },
     {
       asset: './assets/portfolio/static-ecommerce.jpg',
-      title: 'Static E Commerce Apps',
-      description: 'Create a simple e commerce static website using html, css, js and bootstrap. Not too many features because it\'s just a static website'
+      title: 'Static E-commerce Apps',
+      description: 'Create a simple e commerce static website using html, css, js and bootstrap. Not too many features because it\'s just a static website',
+      url: 'https://github.com/afifalfiano/alfiano-store'
     },
     {
       asset: './assets/portfolio/kabar-bola.jpg',
       title: 'Football Information Apps',
-      description: 'Create a simple football schedule website with native javascript, pwa, and webpack. Don\'t forget to use push notifications with the help of firebase to push messages.'
-    }
+      description: 'Create a simple football schedule website with native javascript, pwa, and webpack. Don\'t forget to use push notifications with the help of firebase to push messages.',
+      url: 'https://github.com/afifalfiano/third-pwa-submission'
+    },
+    {
+      asset: 'https://restaurant-responsive-xi.vercel.app/images/hero-image-large.jpg',
+      title: 'Restaurant Apps',
+      description: 'Submission on course become frontend expert developer on dicoding with pwa, indexdb, testing and optimezing ',
+      url: 'https://github.com/afifalfiano/restaurant-responsive'
+    },
+    {
+      asset: './assets/portfolio/fast-wa.png',
+      title: 'Fast-WA',
+      description: 'Simply app to contact other people with whatsapp without saving number',
+      url: 'https://github.com/afifalfiano/unsave-contact-wa'
+    },
+    {
+      asset: './assets/portfolio/pilarng.png',
+      title: 'PilarNg',
+      description: 'Contributor component chips and font pilar on pilarng.github.io',
+      url: 'https://pilarng.github.io/'
+    },
   ];
   constructor() { }
 
